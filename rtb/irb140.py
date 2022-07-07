b054c971c542bc10d98128fa2a8c81551ca2ad85
Cristian Chitiva <cychitivav@@unal.edu.co>
2022-07-06 21:46:23 -0500

    feat: finish lab

--- rtb/irb140.py
+++ rtb/irb140.py
@@ -6,13 +6,15 @@ import numpy as np
 
 class irb140(rtb.DHRobot):
     def __init__(self):
+        self.L = [rtb.DHLink(d=0.352, a=0.07, alpha=-np.pi/2),
+                  rtb.DHLink(d=0,     a=0.36, alpha=0, offset=-np.pi/2),
+                  rtb.DHLink(d=0,     a=0,    alpha=-np.pi/2),
+                  rtb.DHLink(d=0.38,  a=0,    alpha=np.pi/2),
+                  rtb.DHLink(d=0,     a=0,    alpha=-np.pi/2),
+                  rtb.DHLink(d=0.065, a=0,    alpha=0)]
+
         super().__init__(
-            [rtb.DHLink(d=0.352, a=0.07, alpha=-np.pi/2),
-             rtb.DHLink(d=0,     a=0.36, alpha=0, offset=-np.pi/2),
-             rtb.DHLink(d=0,     a=0,    alpha=-np.pi/2),
-             rtb.DHLink(d=0.38,  a=0,    alpha=np.pi/2),
-             rtb.DHLink(d=0,     a=0,    alpha=-np.pi/2),
-             rtb.DHLink(d=0.065, a=0,    alpha=0)],
+            self.L,
             name="irb140",
             tool=SE3(trnorm(transl([0.012254, -0.000002, 0.214736])
                      @ r2t(q2r(([0.923879533, 0, 0.382683432, 0])))))
@@ -35,13 +37,22 @@ class irb140(rtb.DHRobot):
         return q
 
     def MoveC(self, T0, Tc, Tf, n):
+        # self.fkine_path(q)
         return self.MoveJ(T0, Tf, n)
+
+    def ikine(self, T, config='ruf'):
+        q = np.ndarray((4,1))
+        THB = self.L[5].A(0)
+
+        q[2] = 
+
+        return [0, 0, 0, 0, 0, 0]
 
 
 if __name__ == "__main__":
     irb140 = irb140()
 
-    Home = irb140.fkine([0,0,0,0,0,0])
+    Home = irb140.fkine([0, 0, 0, 0, 0, 0])
     wobj = SE3(trnorm(transl([0.653753, 0.399386, 0.025]) @
                r2t(q2r([0.363649594, 0, 0, 0.931535814]))))
 
@@ -97,24 +108,26 @@ if __name__ == "__main__":
 
     # Path planning
     n = 4
-    qs = np.vstack((irb140.MoveJ(Home, wobj*P5, n),  # Brayan
-                    irb140.MoveL(wobj*P5, wobj*P2, n),
-                    irb140.MoveL(wobj*P2, wobj*P1, n),
-                    irb140.MoveC(wobj*P1, wobj*P6, wobj*P5, n),
-                    irb140.MoveC(wobj*P5, wobj*P7, wobj*P4, n),
-                    irb140.MoveL(wobj*P4, wobj*P3, n),
-                    irb140.MoveL(wobj*P3, wobj*P2, n),
-                    irb140.MoveL(wobj*P2, wobj*P7u, n),
-                    irb140.MoveJ(wobj*P7u, wobj*P8, n),  # Julian
-                    irb140.MoveL(wobj*P8, wobj*P9, n),
-                    irb140.MoveL(wobj*P9, wobj*P10, n),
-                    irb140.MoveC(wobj*P10, wobj*P11, wobj*P12, n),
-                    irb140.MoveL(wobj*P12, wobj*P12u, n),
-                    irb140.MoveJ(wobj*P12u, wobj*P13, n),  # Cristian
-                    irb140.MoveC(wobj*P13, wobj*P14, wobj*P15, n),
-                    irb140.MoveC(wobj*P15, wobj*P16, wobj*P17, n),
-                    irb140.MoveC(wobj*P17, wobj*P18, wobj*P19, n),
-                    irb140.MoveJ(wobj*P19, Home, n)))
+    # qs = np.vstack((irb140.MoveL(Home, wobj*P5, n),  # Brayan
+    #                 irb140.MoveL(wobj*P5, wobj*P2, n),
+    #                 irb140.MoveL(wobj*P2, wobj*P1, n),
+    #                 irb140.MoveC(wobj*P1, wobj*P6, wobj*P5, n),
+    #                 irb140.MoveC(wobj*P5, wobj*P7, wobj*P4, n),
+    #                 irb140.MoveL(wobj*P4, wobj*P3, n),
+    #                 irb140.MoveL(wobj*P3, wobj*P2, n),
+    #                 irb140.MoveL(wobj*P2, wobj*P7u, n),
+    #                 irb140.MoveJ(wobj*P7u, wobj*P8, n),  # Julian
+    #                 irb140.MoveL(wobj*P8, wobj*P9, n),
+    #                 irb140.MoveL(wobj*P9, wobj*P10, n),
+    #                 irb140.MoveC(wobj*P10, wobj*P11, wobj*P12, n),
+    #                 irb140.MoveL(wobj*P12, wobj*P12u, n),
+    #                 irb140.MoveJ(wobj*P12u, wobj*P13, n),  # Cristian
+    #                 irb140.MoveC(wobj*P13, wobj*P14, wobj*P15, n),
+    #                 irb140.MoveC(wobj*P15, wobj*P16, wobj*P17, n),
+    #                 irb140.MoveC(wobj*P17, wobj*P18, wobj*P19, n),
+    #                 irb140.MoveJ(wobj*P19, Home, n)))
 
-    irb140.plot(qs, dt=1, jointaxes=True,movie='BJC.gif')
+    qs = irb140.ikine(Home)
+
+    irb140.plot(qs, dt=1, jointaxes=True, movie='BJC.gif')
     input()
